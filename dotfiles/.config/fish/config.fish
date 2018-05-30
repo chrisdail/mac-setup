09b30889303dd6e5f55a82bddca61f09f2afe6a3
Chris Dail <Chris.Dail@akiri.com>
2018-05-30 16:37:53 -0300

    Added epoxy env

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -5,7 +5,7 @@ set -gx EDITOR vim
 set -gx GOPATH ~/code/switch/go
 set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_152.jdk/Contents/Home
 set -gx ANDROID_HOME ~/Library/Android/sdk
-set -gx EPOXY_VAULT_MODE shared
+set -gx EPOXY_ENV dev-virginia
 set -gx EPOXY_QUICK true
 set -gx EPOXY_TOPOLOGY basic
 
