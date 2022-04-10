b2b2c5ccf94427ad8b634fded228f0c575256a71
Chris Dail <chris@motryx.com>
2022-04-09 22:43:13 -0300

    Latest changes for new laptop

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -2,18 +2,13 @@ set fish_greeting ""
 
 # Env
 set -gx EDITOR vim
-set -gx ANDROID_HOME ~/Library/Android/sdk
-
-set -gx EPOXY_ENV development
-set -gx EPOXY_TOPOLOGY basic
-set -gx EPOXY_QUICK true
 set -gx DOCKER_BUILDKIT 1
 
 # PATH
-set -gx PATH ~/bin ~/code/switch/bin ~/code/switch/devops/bin /usr/local/opt/python/libexec/bin ~/.cargo/bin $PATH
+set -gx PATH /opt/homebrew/bin ~/bin $PATH
 
 # Aliases
-alias vi='/usr/local/bin/vim'
+alias vi='/opt/homebrew/bin/vim'
 alias gcleanup='git branch --merged | grep -v "\*" | grep -v "master" | grep -v "staging" | xargs -n 1 git branch -d'
 alias brokenlinks='find ./ -type l -exec file {} \; |grep broken'
 
